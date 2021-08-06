71b35991d13556cd601bf70bd32f445bd20b01bc
malithiperera <62257561+malithiperera@users.noreply.github.com>
2021-08-06 21:06:59 +0530

    First Commit

---
+++ CiserCipher.scala
@@ -0,0 +1,53 @@
+package CiserCipher
+
+object CiserCipher {
+  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+//  val E: (Char, Int, String) => Char = (c: Char, key: Int, a: String) =>
+//    Predef.augmentString(a)(i = (a.indexOf(c.toUpper) + key) % a.length)
+val E=(c:Char,key:Int,a:String)=> a((a.indexOf(c.toUpper)+key)%a.size)
+
+
+  val D: (Char, Int, String) => Char = (c: Char, key: Int, a: String) => a((a.indexOf(c.toUpper) - key) % a.length)
+
+  val cipher: ((Char, Int, String) => Char, String, Int, String) => String = (algo: (Char, Int, String) =>
+    Char, s: String, key: Int, a: String) =>
+    s.map(algo(_, key, a))
+
+
+
+  def main(args: Array[String]): Unit = {
+println("Select Choice:")
+    println("1:Encrpt")
+    println("2.Decrpyt")
+
+    val choice = scala.io.StdIn.readInt()
+    if(choice==1)
+      {
+        println("Enter the key value:")
+        val b = scala.io.StdIn.readInt()
+        println("Enter the string to Encrypt")
+        val s=scala.io.StdIn.readLine()
+        val ct: String = cipher(E, s, b, alphabet)
+        println(" String after encrypted: " + ct)
+        val pt: String = cipher(D, ct, b, alphabet)
+        println(" String again after decrypted: "+ pt)
+      }
+
+    else if(choice==2){
+      println("Enter the key value:")
+      val d = scala.io.StdIn.readInt()
+      println("Enter the string to Decrypt")
+      val dec=scala.io.StdIn.readLine()
+      val pt: String = cipher(D, dec, d, alphabet)
+      println(" String again after decrypted: "+ pt)
+    }
+
+    else{
+      println("Invalid Choice")
+    }
+
+
+
+  }
+}
