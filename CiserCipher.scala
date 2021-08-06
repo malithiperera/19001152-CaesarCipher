8a049508afc8e540719d3c18cba2eb844dc4f37a
malithiperera <62257561+malithiperera@users.noreply.github.com>
2021-08-06 22:29:39 +0530

    second commit

--- CiserCipher.scala
+++ CiserCipher.scala
@@ -4,10 +4,11 @@ object CiserCipher {
   val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-val E=(c:Char,key:Int,a:String)=> a((a.indexOf(c.toUpper)+key)%a.size)
+  val E=(c:Char,key:Int,a:String)=> a((a.indexOf(c.toUpper)+key)%a.size)
 
 
-  val D: (Char, Int, String) => Char = (c: Char, key: Int, a: String) => a((a.indexOf(c.toUpper) - key) % a.length)
+  val D=(c:Char,key:Int,a:String)=>
+    a((a.indexOf(c.toUpper)-key)%a.size)
 
   val cipher: ((Char, Int, String) => Char, String, Int, String) => String = (algo: (Char, Int, String) =>
     Char, s: String, key: Int, a: String) =>
@@ -16,30 +17,33 @@ val E=(c:Char,key:Int,a:String)=> a((a.indexOf(c.toUpper)+key)%a.size)
 
 
   def main(args: Array[String]): Unit = {
-println("Select Choice:")
+    println("Select Choice:")
     println("1:Encrpt")
     println("2.Decrpyt")
 
     val choice = scala.io.StdIn.readInt()
     if(choice==1)
-      {
-        println("Enter the key value:")
-        val b = scala.io.StdIn.readInt()
-        println("Enter the string to Encrypt")
-        val s=scala.io.StdIn.readLine()
-        val ct: String = cipher(E, s, b, alphabet)
-        println(" String after encrypted: " + ct)
-        val pt: String = cipher(D, ct, b, alphabet)
-        println(" String again after decrypted: "+ pt)
-      }
+    {
+      println("Enter the key value:")
+      val b = scala.io.StdIn.readInt()
+      println("Enter the string to Encrypt")
+      val s=scala.io.StdIn.readLine()
+      val ct: String = cipher(E, s, b, alphabet)
+      println(" String after encrypted: " + ct)
+      val pt: String = cipher(D, ct, b, alphabet)
+      println(" String again after decrypted: "+ pt)
+    }
 
     else if(choice==2){
-      println("Enter the key value:")
+     println("Enter the key value:")
       val d = scala.io.StdIn.readInt()
-      println("Enter the string to Decrypt")
-      val dec=scala.io.StdIn.readLine()
+     println("Enter the string to Decrypt")
+   val dec=scala.io.StdIn.readLine()
+
       val pt: String = cipher(D, dec, d, alphabet)
-      println(" String again after decrypted: "+ pt)
+     
+
+     println(" String again after decrypted: "+ pt)
     }
 
     else{
